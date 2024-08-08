95e229ff2d065261cdf14b0770237013d0d44b0f
sualah <sualahabdullai@gmail.com>
2024-08-08 21:26:16 +0000

    updated parser

--- src/interpreter.ts
+++ src/interpreter.ts
@@ -4,7 +4,43 @@ import { TokenType } from "tokenType";
 export class Interpreter implements Expr.Visitor<Object> {
 
     visitBinaryExpr(expr: Expr.Binary): Object {
-        return this.parenthesize(expr.operator.lexeme, expr.left, expr.right);
+      let left: Object = this.evaluate(expr.left);
+      let right: Object = this.evaluate(expr.right);
+      switch (expr.operator.type) {
+        case TokenType.GREATER:
+            return Number(left) > Number(right);
+        case TokenType.GREATER_EQUAL:
+            return Number(left) >= Number(right);
+        case TokenType.LESS:
+            return Number(left) < Number(right);
+        case TokenType.LESS_EQUAL:
+            return Number(left) <= Number(right);
+        case TokenType.BANG_EQUAL: return !this.isEqual(left, right);
+        case TokenType.EQUAL_EQUAL: return this.isEqual(left, right);
+        case TokenType.MINUS:
+          return Number(left) - Number(right);
+        case TokenType.PLUS:
+            if ( typeof left === 'number' &&  typeof right === 'number') {
+              return Number(left) + Number(right);
+            }
+            if (typeof left === 'string' &&  typeof right === 'string') {
+              return String(left) + String(right);
+  }
+            break;
+        case TokenType.SLASH:
+          return Number(left) / Number(right);
+        case TokenType.STAR:
+          return Number(left) * Number(right);
+      }
+
+        return Object(null);
+    }
+
+
+    isEqual(a: Object, b: Object): boolean {
+        if (a === null && b == null) return true;
+        if (a == null) return false;
+        return a === b;
     }
 
     visitGroupingExpr(expr: Expr.Grouping): Object {
@@ -23,12 +59,20 @@ export class Interpreter implements Expr.Visitor<Object> {
     visitUnaryExpr(expr: Expr.Unary): Object {
         let right: Object = this.evaluate(expr.right);
       switch (expr.operator.type) {
+        case TokenType.BANG:
+          return !this.isTruthy(right);
         case TokenType.MINUS:
           return -right;
         default:
             return Object(null)
       }
       //  return this.parenthesize(expr.operator.lexeme, expr.right);
+    }
+
+    isTruthy(object: Object) : boolean {
+       if (object == null) return false;
+      if (object instanceof Boolean) return Boolean(object);
+      return true;
     }
 
     private parenthesize(name: string, ...exprs: Expr[]): string {
